4fa4d13fd1cefb098386811670c4cd60447c9a85
Igor Malyarov <igor1309@gmail.com>
2022-10-06 16:15:15 +0300

    add Xcode project

---
+++ App/EssentialGhibli/EssentialGhibliApp.swift
@@ -0,0 +1,17 @@
+//
+//  EssentialGhibliApp.swift
+//  EssentialGhibli
+//
+//  Created by Igor Malyarov on 06.10.2022.
+//
+
+import SwiftUI
+
+@main
+struct EssentialGhibliApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
