917e98e3fbc231e6cb593fbe6f4ba0f59a2c395c
Igor Malyarov <igor1309@gmail.com>
2022-10-07 21:39:25 +0300

    test  snapshot LoadableResourceView with error

--- Tests/GenericResourceViewTests/GenericResourceViewTests.swift
+++ Tests/GenericResourceViewTests/GenericResourceViewTests.swift
@@ -20,26 +20,19 @@ final class GenericResourceViewTests: XCTestCase {
         assert(snapshot: view, locale: .ru_RU, record: record)
     }
     
-//    func test_snapshotLoadableResourceView_empty() {
-//        let view = ghibliListView(.empty)
-//        
-//        assert(snapshot: view, locale: .en_US, record: record)
-//        assert(snapshot: view, locale: .ru_RU, record: record)
-//    }
-//    
 //    func test_snapshotLoadableResourceView_list() {
 //        let view = ghibliListView(.list([.castleInTheSky, .kikisDeliveryService]))
 //        
 //        assert(snapshot: view, locale: .en_US, record: record)
 //        assert(snapshot: view, locale: .ru_RU, record: record)
 //    }
-//    
-//    func test_snapshotLoadableResourceView_error() {
-//        let view = ghibliListView(.error(APIError()))
-//        
-//        assert(snapshot: view, locale: .en_US, record: record)
-//        assert(snapshot: view, locale: .ru_RU, record: record)
-//    }
+    
+    func test_snapshotLoadableResourceView_error() {
+        let view = loadableResourceView(.error(anyError()))
+        
+        assert(snapshot: view, locale: .en_US, record: record)
+        assert(snapshot: view, locale: .ru_RU, record: record)
+    }
     
     // MARK: - Helpers
     typealias StringState = ResourceState<String, Error>
@@ -56,4 +49,12 @@ final class GenericResourceViewTests: XCTestCase {
 extension Locale {
     static let en_US: Self = .init(identifier: "en-US")
     static let ru_RU: Self = .init(identifier: "ru-RU")
+}
+
+func anyError(message: String = "any error") -> AnyError {
+    .init(message: message)
+}
+
+struct AnyError: Error {
+    let message: String
 }
