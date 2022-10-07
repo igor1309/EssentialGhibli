cec92fabc9f2f5ef1609ccd3a17de23f466791e3
Igor Malyarov <igor1309@gmail.com>
2022-10-07 11:29:22 +0300

    add `URLSessionHTTPClient`

--- Sources/EssentialGhibliHTTPClient/HTTPClient.swift
+++ Sources/EssentialGhibliHTTPClient/HTTPClient.swift
@@ -7,6 +7,15 @@
 
 import Foundation
 
-public final class HTTPClient {
-    public init() {}
+public protocol HTTPClientTask {
+    func cancel()
+}
+
+public protocol HTTPClient {
+    typealias Result = Swift.Result<(Data, HTTPURLResponse), Error>
+    
+    /// The completion handler can be invoked in any thread.
+    /// Clients are responsible to dispatch to appropriate threads, if needed.
+    @discardableResult
+    func get(from url: URL, completion: @escaping (Result) -> Void) -> HTTPClientTask
 }
---
+++ Sources/EssentialGhibliHTTPClient/URLSessionHTTPClient.swift
@@ -0,0 +1,46 @@
+//
+//  URLSessionHTTPClient.swift
+//  
+//
+//  Created by Igor Malyarov on 07.10.2022.
+//
+
+import Foundation
+
+public final class URLSessionHTTPClient: HTTPClient {
+    public typealias Result = Swift.Result<(Data, HTTPURLResponse), Error>
+    
+    private let session: URLSession
+    
+    public init(session: URLSession) {
+        self.session = session
+    }
+    
+    private struct UnexpectedValuesRepresentation: Error {}
+    
+    private struct URLSessionTaskWrapper: HTTPClientTask {
+        let wrapped: URLSessionTask
+        
+        func cancel() {
+            wrapped.cancel()
+        }
+    }
+    
+    @discardableResult
+    public func get(from url: URL, completion: @escaping (Result) -> Void) -> HTTPClientTask {
+        let task = session.dataTask(with: url) { data, response, error in
+            completion(Result {
+                if let error = error {
+                    throw error
+                } else if let data = data, let response = response as? HTTPURLResponse {
+                    return (data, response)
+                } else {
+                    throw UnexpectedValuesRepresentation()
+                }
+            })
+        }
+        
+        task.resume()
+        return URLSessionTaskWrapper(wrapped: task)
+    }
+}
