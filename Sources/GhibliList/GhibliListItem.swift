5030753b53608a8902ad91d969f0e08915e24375
Igor Malyarov <igor1309@gmail.com>
2022-10-07 14:05:22 +0300

    fix property names in `GhibliList`

--- Sources/GhibliList/GhibliListItem.swift
+++ Sources/GhibliList/GhibliListItem.swift
@@ -11,14 +11,14 @@ public struct GhibliListItem: Identifiable, Hashable {
     public let id: UUID
     public let title: String
     public let description: String
-    public let image: URL
-    public let film: URL
+    public let imageURL: URL
+    public let filmURL: URL
     
-    public init(id: UUID, title: String, description: String, image: URL, film: URL) {
+    public init(id: UUID, title: String, description: String, imageURL: URL, filmURL: URL) {
         self.id = id
         self.title = title
         self.description = description
-        self.image = image
-        self.film = film
+        self.imageURL = imageURL
+        self.filmURL = filmURL
     }
 }
