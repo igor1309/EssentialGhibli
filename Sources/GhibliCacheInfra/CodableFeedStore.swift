fe5b45b6bfd02902bdea9b724fd55fb72dd953ea
Igor Malyarov <igor1309@gmail.com>
2022-10-10 14:40:43 +0300

    extract `LocalFilm` into separate file

--- Sources/GhibliCacheInfra/CodableFeedStore.swift
+++ Sources/GhibliCacheInfra/CodableFeedStore.swift
@@ -8,28 +8,6 @@
 import GhibliCache
 import Foundation
 
-public struct LocalFilm: Equatable {
-    public let id: UUID
-    public let title: String
-    public let description: String
-    public let imageURL: URL
-    public let filmURL: URL
-    
-    public init(
-        id: UUID,
-        title: String,
-        description: String,
-        imageURL: URL,
-        filmURL: URL
-    ) {
-        self.id = id
-        self.title = title
-        self.description = description
-        self.imageURL = imageURL
-        self.filmURL = filmURL
-    }
-}
-
 public final class CodableFeedStore: FeedStore {
     private let storeURL: URL
     
---
+++ Sources/GhibliCacheInfra/LocalFilm.swift
@@ -0,0 +1,30 @@
+//
+//  LocalFilm.swift
+//  
+//
+//  Created by Igor Malyarov on 10.10.2022.
+//
+
+import Foundation
+
+public struct LocalFilm: Equatable {
+    public let id: UUID
+    public let title: String
+    public let description: String
+    public let imageURL: URL
+    public let filmURL: URL
+    
+    public init(
+        id: UUID,
+        title: String,
+        description: String,
+        imageURL: URL,
+        filmURL: URL
+    ) {
+        self.id = id
+        self.title = title
+        self.description = description
+        self.imageURL = imageURL
+        self.filmURL = filmURL
+    }
+}
