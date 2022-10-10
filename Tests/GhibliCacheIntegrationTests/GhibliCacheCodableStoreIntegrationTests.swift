8636b37aa16c0ea8358e7e57110a6c5606714980
Igor Malyarov <igor1309@gmail.com>
2022-10-11 02:34:18 +0300

    copy CodableStoreTest setup from `CodableFeedStoreTests`

--- Tests/GhibliCacheIntegrationTests/GhibliCacheCodableStoreIntegrationTests.swift
+++ Tests/GhibliCacheIntegrationTests/GhibliCacheCodableStoreIntegrationTests.swift
@@ -11,4 +11,42 @@ import XCTest
 
 final class GhibliCacheCodableStoreIntegrationTests: XCTestCase {
     
+    override func setUp() {
+        super.setUp()
+        setupEmptyStoreState()
+    }
+    
+    override func tearDown() {
+        super.tearDown()
+        undoStoreSideEffects()
+    }
+    
+    // MARK: - Helpers
+    
+    private func makeSUT(
+        file: StaticString = #file,
+        line: UInt = #line
+    ) -> CodableFeedStore {
+        let sut = CodableFeedStore(storeURL: testStoreURL())
+        trackForMemoryLeaks(sut, file: file, line: line)
+        return sut
+    }
+    
+    private func testStoreURL() -> URL {
+        FileManager.default.urls(for: .cachesDirectory, in: .userDomainMask).first!
+            .appendingPathComponent("\(type(of: self)).store")
+    }
+    
+    private func setupEmptyStoreState() {
+        clearArtifacts()
+    }
+    
+    private func undoStoreSideEffects() {
+        clearArtifacts()
+    }
+    
+    private func clearArtifacts() {
+        try? FileManager.default.removeItem(at: testStoreURL())
+    }
+
 }
