4b9d87904f56ba04e2119b1da5cb73fd8878a437
Igor Malyarov <igor1309@gmail.com>
2022-10-11 15:00:36 +0300

    add `test_load_shouldRequestStoreRetrieval` and `test_load_shouldDeliverErrorOnRetrievalFailure`

--- Tests/GhibliCacheTests/FilmImageDataCacheTests.swift
+++ Tests/GhibliCacheTests/FilmImageDataCacheTests.swift
@@ -8,7 +8,7 @@
 import XCTest
 
 protocol FilmDataStore {
-    
+    func retrieve() throws
 }
 
 final class FilmImageDataCache {
@@ -16,6 +16,10 @@ final class FilmImageDataCache {
     
     init(store: FilmDataStore) {
         self.store = store
+    }
+    
+    func load() throws {
+        try store.retrieve()
     }
 }
 
@@ -25,6 +29,28 @@ final class FilmImageDataCacheTests: XCTestCase {
         let (_, store) = makeSUT()
         
         XCTAssert(store.messages.isEmpty)
+    }
+
+    func test_load_shouldRequestStoreRetrieval() throws {
+        let (sut, store) = makeSUT()
+        
+        try sut.load()
+        
+        XCTAssertEqual(store.messages, [.retrieve])
+    }
+    
+    func test_load_shouldDeliverErrorOnRetrievalFailure() throws {
+        let (sut, store) = makeSUT()
+        store.completeRetrieval(with: .failure(anyError()))
+
+        do {
+            try sut.load()
+            XCTFail("Expected error")
+        } catch let error as AnyError {
+            XCTAssertEqual(error, AnyError())
+        } catch {
+            XCTFail("Expected AnyError, got \(error.localizedDescription)")
+        }
     }
     
     // MARK: - Helpers
@@ -43,10 +69,22 @@ final class FilmImageDataCacheTests: XCTestCase {
     }
     
     private final class StoreStub: FilmDataStore {
+        typealias RetrievalResult = Result<(), Error>
+        
+        private var retrievalResult: RetrievalResult?
         private(set) var messages = [Message]()
         
+        func completeRetrieval(with result: RetrievalResult) {
+            self.retrievalResult = result
+        }
+        
+        func retrieve() throws {
+            messages.append(.retrieve)
+            try retrievalResult?.get()
+        }
+        
         enum Message {
-            
+            case retrieve
         }
     }
 }
