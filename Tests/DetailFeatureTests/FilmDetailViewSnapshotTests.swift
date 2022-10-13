12b7214e2487f1cf4285bdd36a06fee6309e6e4c
Igor Malyarov <igor1309@gmail.com>
2022-10-14 01:29:59 +0300

    tweak `FilmDetailViewSnapshotTests` to have more options

--- Tests/DetailFeatureTests/FilmDetailViewSnapshotTests.swift
+++ Tests/DetailFeatureTests/FilmDetailViewSnapshotTests.swift
@@ -12,10 +12,48 @@ import XCTest
 final class FilmDetailViewSnapshotTests: XCTestCase {
     let record = false
     
-    func test_snapshot_FilmDetailView() {
-        let view = FilmDetailView(film: .castleInTheSky) { _ in Color.red }
+    func test_snapshot_FilmDetailView_red() {
+        let sut = makeSUT()
+
+        assert(snapshot: sut, locale: .en_US, record: record)
+    }
+    
+    func test_snapshot_FilmDetailView_orange() {
+        let sut = makeSUT(color: .orange)
+
+        assert(snapshot: sut, locale: .en_US, record: record)
+    }
+    
+    func test_snapshot_FilmDetailView_triangle() {
+        let sut = makeSUT {
+            Image(systemName: "exclamationmark.triangle")
+                .resizable()
+                .scaledToFit()
+        }
         
-        assert(snapshot: view, locale: .en_US, record: record)
+        assert(snapshot: sut, locale: .en_US, record: record)
+    }
+    
+    // MARK: - Helpers
+    
+    private func makeSUT(
+        color: Color = .red,
+        file: StaticString = #file,
+        line: UInt = #line
+    ) -> some View {
+        makeSUT(poster: {
+            color.aspectRatio(1, contentMode: .fit)
+        }, file: file, line:  line)
+    }
+    
+    private func makeSUT(
+        poster: @escaping () -> some View,
+        file: StaticString = #file,
+        line: UInt = #line
+    ) -> some View {
+        let sut = FilmDetailView(film: .castleInTheSky) { _ in poster() }
+        
+        return sut
     }
 }
 
